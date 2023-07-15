eda31e1ffdfb3044e385dc2002d8da951b75a4e4
Louise Raffray <raffraylouise@gmail.com>
2023-07-15 16:49:20 +0100

    sign up styling

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -35,18 +35,22 @@ export default class SignUpForm extends Component {
       render() {
         const disable = this.state.password !== this.state.confirm;
         return (
-          <div>
-            <div className="form-container">
+          <div className="login-parent">
+          <div className="login-form-container">
+          <h4 className="login-header">Let's start by getting you signed up</h4>
               <form autoComplete="off" onSubmit={this.handleSubmit}>
-                <label>Name</label>
+                <label className='email'>Name *</label>
                 <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-                <label>Email</label>
-                <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-                <label>Password</label>
+                <label className="email">Email Address *</label>
+                <input type="text" name="email" value={this.state.email} onChange={this.handleChange} required />
+                <label className='password'>Password *</label>
                 <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-                <label>Confirm</label>
+                <label className='password'>Confirm Password *</label>
                 <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                <h3 className='required-fields'>* Required Fields</h3>
+                <div className="signup-button-container">
                 <button type="submit" disabled={disable}>SIGN UP</button>
+                </div>
               </form>
             </div>
             <p className="error-message">&nbsp;{this.state.error}</p>
