96b0e383ed2146009ed530f38a01858d51780e9e
Louise Raffray <raffraylouise@gmail.com>
2023-07-16 13:34:58 +0100

    asset modal is styled, just need to get button functionality working

--- src/components/AssetModal/AssetModal.jsx
+++ src/components/AssetModal/AssetModal.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
-import NewAssetForm from '../NewAssetForm/NewAssetForm';
+import EditAssetForm from '../EditAssetForm/EditAssetForm';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import "../css/EditAssetForm.css"
 
 export default function NewAssetFormModal({ open, handleClose, onAssetChange, asset, handleSave, user }) {
     const darkTheme = createTheme({
@@ -13,19 +13,25 @@ export default function NewAssetFormModal({ open, handleClose, onAssetChange, as
       
     return (
         <ThemeProvider theme={darkTheme}>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Add a New Asset</DialogTitle>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" className="dialogContainer">
         <DialogContent>
-            <NewAssetForm 
-                onAssetChange={onAssetChange} user={user} asset={asset} onSave={handleSave}/>
+        <EditAssetForm 
+            onAssetChange={onAssetChange} 
+            user={user} 
+            asset={asset} 
+            onSave={handleSave} 
+            onCancel={handleClose} 
+        />
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose} color="secondary">
+            <div className="button-container">
+                    <button onClick={handleClose} color="secondary" className="cancel-button">
                     Cancel
-                </Button>
-                <Button onClick={handleSave} color="primary">
+                    </button>
+                    <Button onClick={handleSave} color="primary" className="asset-button">
                     Save
-                </Button>
+                    </Button>
+                 </div>
             </DialogActions>
         </Dialog>
         </ThemeProvider>
---
+++ src/components/EditAssetForm/EditAssetForm.jsx
@@ -0,0 +1,209 @@
+import {Component} from 'react';
+import axios from 'axios';
+import Autosuggest from "react-autosuggest";
+import "../css/EditAssetForm.css"
+import "../css/LoginForm.css"
+import { Button } from '@mui/material';
+
+
+
+export default class EditAssetForm extends Component {
+    state = {
+        ticker: '',
+        units: '',
+        sector: '',
+        tickerSuggestions: [],
+        error: '',
+        successMessage: '',
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.asset !== prevProps.asset) {
+            if (this.props.asset) {
+                this.setState(this.props.asset);
+            } else {
+                this.setState({
+                ticker: '', 
+                units: '', 
+                sector: '', 
+                tickerSuggestions: [], 
+                error: '',
+                successMessage: '',});
+            }
+        }
+    }
+
+    handleChange = (evt) => {
+        const newState = {
+          [evt.target.name]: evt.target.value,
+          error: '',
+          successMessage: '',
+        };
+        this.setState(newState, () => {
+            if (this.props.onAssetChange) {
+                this.props.onAssetChange(newState);
+            }
+        });
+    };
+    
+
+    handleSubmit = async (evt) => {
+        evt.preventDefault();
+        try {
+          const formData = {...this.state};
+          delete formData.confirm;
+          delete formData.error;
+    
+          console.log(this.props.user._id)
+          console.log(formData);
+          // Check if this is an edit operation
+          if (this.props.asset && this.props.asset._id) {
+            // Update existing asset
+            await axios.put(`/api/portfolio/${this.props.user._id}/asset/${this.props.asset._id}`, formData);
+          } else {
+            // Create new asset
+            await axios.post(`/api/portfolio/${this.props.user._id}/asset`, formData);
+          }
+
+          // Reset form fields
+    this.setState({
+      ticker: '',
+      units: '',
+      sector: 'Energy',
+      tickerSuggestions: [],
+      error: '',
+      successMessage: 'Asset successfully added to your portfolio.',
+    });
+
+          // Call onSave function if provided (this is to support modal closing and other post-save operations)
+          if (this.props.onSave) {
+            this.props.onSave();
+          }
+
+          // Clear success message after 5 seconds
+      setTimeout(() => {
+        this.setState({ successMessage: '' });
+      }, 5000);
+
+        } catch(error) {
+          console.error(error);
+          this.setState({ error: 'Asset was not added. Please try again.' });
+        }
+      };
+
+      getTickerSuggestions = async (value) => {
+        try {
+          const response = await axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${process.env.REACT_APP_ALPHA_API_KEY}`, {
+              headers: {
+                //   'User-Agent': 'request'
+              }
+          });
+          
+          if (response.data && response.data['bestMatches']) {
+            this.setState({ tickerSuggestions: response.data['bestMatches'] });
+        } else {
+            this.setState({ tickerSuggestions: [] });
+        }
+    } catch (error) {
+        console.log('Ticker API error:', error);
+        this.setState({ tickerSuggestions: [] });
+    }
+};
+      
+
+    onTickerSuggestionsFetchRequested = ({ value }) => {
+        this.getTickerSuggestions(value);
+    };
+
+    onTickerSuggestionsClearRequested = () => {
+        this.setState({
+        tickerSuggestions: []
+        });
+    }; 
+        // When suggestion is clicked, Autosuggest needs to populate the input field based on the clicked suggestion.
+    getTickerSuggestionValue = (suggestion) => suggestion['1. symbol'];
+
+    renderTickerSuggestion = (suggestion) => (
+        <div key={suggestion['1. symbol']} className="suggestion-item">
+        {`${suggestion['1. symbol']} - ${suggestion['2. name']}`}
+      </div>
+    );
+    
+    handleTickerChange = (event, { newValue }) => {
+        this.setState({
+        ticker: newValue
+        });
+    };
+
+      render() {
+        const { ticker, tickerSuggestions, successMessage } = this.state;
+
+
+        // Autosuggest will pass through all these props to the input field.
+    const tickerInputProps = {
+        placeholder: "Type a ticker",
+        value: ticker,
+        onChange: this.handleTickerChange,
+        className: "form-control"
+      };
+  
+
+        return (
+          <div className="login-parent">
+            <div className="edit-form-container">
+              <form autoComplete="off" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+          <h4 className="edit-header">EDIT ASSET</h4>
+                <label className='email'>Ticker</label>
+                <Autosuggest
+                  className="ticker-suggest"
+                    suggestions={tickerSuggestions}
+                    onSuggestionsFetchRequested={this.onTickerSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onTickerSuggestionsClearRequested}
+                    getSuggestionValue={this.getTickerSuggestionValue}
+                    renderSuggestion={this.renderTickerSuggestion}
+                    inputProps={tickerInputProps}
+                    name="email"
+                    theme={{
+                        suggestionsContainer: 'suggestions-container',
+                        suggestion: 'suggestion-item',
+                        suggestionHighlighted: 'suggestion-item--highlighted'
+                      }}                
+                />
+                 </div>
+                 <div className="form-group">
+                <label className='email'>Units</label>
+                <input type="text" name="units"  value={this.state.units} onChange={this.handleChange} required />
+                </div>
+                <label className='email'>Sector</label>
+                <select name="sector" className="asset-sector" value={this.state.sector} onChange={this.handleChange} required>
+                    <option value="Energy">Energy</option>
+                    <option value="Materials">Materials</option>
+                    <option value="Industrials">Industrials</option>
+                    <option value="Utilities">Utilities</option>
+                    <option value="Healthcare">Healthcare</option>
+                    <option value="Financials">Financials</option>
+                    <option value="Consumer Discretionary">Consumer Discretionary</option>
+                    <option value="Consumer Staples">Consumer Staples</option>
+                    <option value="Information Technology">Information Technology</option>
+                    <option value="Communication Services">Communication Services</option>
+                    <option value="Real Estate">Real Estate</option>
+                </select>
+                <h3 className='required-fields'>* Required Fields</h3>
+                <div className="button-container">
+                    <button onClick={this.props.onCancel} color="secondary" className="cancel-button">
+                    Cancel
+                    </button>
+                    <Button onClick={this.props.handleSave} color="primary" className="asset-button">
+                    Save
+                    </Button>
+                 </div>
+              </form>
+              {successMessage && <p className="success-message">{successMessage}</p>}
+            <p className="error-message">&nbsp;{this.state.error}</p>
+          </div>
+          </div>
+        );
+      }
+      
+    }
